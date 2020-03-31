72d8b753888090b872ee495c1108b5b86521ea58
prodxxx <dave.miranda@gmail.com>
2020-03-30 20:56:44 -0400

    Delete code.js
    was testing foreach arrach

--- code.js
+++
@@ -1,5 +0,0 @@
-var friends = ["Mike", "Stacy", "Andy", "Rick"];
-
-friends.forEach(function (eachName, index) {
-  console.log(index + 1 + ". " + eachName); // 1. Mike, 2. Stacy, 3. Andy, 4. Rick
-});
